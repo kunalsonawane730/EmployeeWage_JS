13ad4db08234e2bda527f15927d24d33a3e6d523
Kunal <kunalsonawane730@gmail.com>
2022-12-25 15:58:50 +0530

    Add: calculating daily employee wage with employee is full or part time by random with JS code

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,6 +1,5 @@
 console.log("******** Welcome To Employee Wage Computation Application *********");
 
-//UC1
 {
     const IS_ABSENT = 0
     let employeeCheck = Math.floor(Math.random() * 10) % 2;
@@ -12,7 +11,6 @@ console.log("******** Welcome To Employee Wage Computation Application *********
     }
 }
 
-//UC2
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -23,21 +21,22 @@ const WAGE_PER_HOUR = 20;
 let employeeHours = 0;
 let employeeCheck = Math.floor(Math.random() * 10) % 3;
 
-switch (employeeCheck) {
+function getWorkingHours(employeeCheck){
 
-    case IS_PART_TIME:
-        employeeHours = PART_TIME_HOURS;
-        console.log("Employee works Part Time");
-        break;
-
-    case IS_FULL_TIME:
-        employeeHours = FULL_TIME_HOURS;
-        console.log("Employee works Full Time");
-        break;
-
-    default:
-        employeeHours = 0;
+    switch (employeeCheck) {
+    
+        case IS_PART_TIME:
+            console.log("Employee works Part Time");
+            return PART_TIME_HOURS;
+            
+        case IS_FULL_TIME:
+            console.log("Employee works Full Time");
+            return FULL_TIME_HOURS;
+            
+        default:
+            return 0;
+    }
 }
-
+employeeHours = getWorkingHours(employeeCheck);
 let employeeWage = employeeHours * WAGE_PER_HOUR;
 console.log("Employee Wage = " + employeeWage);
